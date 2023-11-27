a46f56d3ee927352c026f9bfa2f8e08d7ec56796
saif-eddine <saifdll02@gmail.com>
2023-11-27 07:51:20 -0800

    1 file

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+defines Rectangle class with a private attrbutes with and height
+args:
+    width (int): width
+    height (int): height
+     Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+"""
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+
+    @property
+    def width(self):
+     """width getter"""
+        return self.__width
+
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.height
+
+    @height.setter
+    """height setter"""
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+
+
