7bd139fb786c51f41f964af7bd5d12aa395a0edc
saif-eddine <saifdll02@gmail.com>
2024-03-26 09:56:05 -0700

    all files

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const url = `https://swapi-api.alx-tools.com/api/${id}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
